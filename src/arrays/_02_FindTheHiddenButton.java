84d5f3446c85b71b7047392a8a8026defddfbb53
root <root@3ba6bb870b97>
2021-01-09 00:57:08 +0000

    Proccesing

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -22,12 +22,14 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	JButton [] arr = new JButton[10];
+	JButton [] arr;
 	//2 create an int variable called hiddenButton
 	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
+	Random rand = new Random();
+	int num;
 	
 	public void start() {
 		window = new JFrame("Find the Button");
@@ -36,9 +38,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
 		String number = JOptionPane.showInputDialog("Enter a positive number: ");
-		int num = Integer.parseInt(number);
+		num = Integer.parseInt(number);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		JButton[] arr = new JButton[num];
+		arr = new JButton[num];
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
@@ -57,7 +59,6 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		//12. Give the user the instructions for the game.
 		JOptionPane.showMessageDialog(null, "Find the hidden button.");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		Random rand = new Random();
 		hiddenButton = rand.nextInt(num);
 		//14. Set the text of the JButton located at hiddenButton to  "ME"
 		arr[hiddenButton].setText("ME");
@@ -79,9 +80,23 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
 		if (buttonClicked == arr[hiddenButton]) {
+		
 			JOptionPane.showMessageDialog(null, "You win");
+			System.exit(0);
+			
 		} else {
 			JOptionPane.showMessageDialog(null, "Try again");
+			hiddenButton = rand.nextInt(num);
+			arr[hiddenButton].setText("ME");
+			window.pack();
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException a) {
+				// TODO Auto-generated catch block
+				a.printStackTrace();
+			}
+			arr[hiddenButton].setText("");
 		}
 		//18. else tell them to try again
 	}
